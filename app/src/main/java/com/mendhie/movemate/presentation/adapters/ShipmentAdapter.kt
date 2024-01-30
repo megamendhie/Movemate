9fbf396c7e9a2d52fc0e97eb17807418d91fc4ee
megamendhie <imendhie@yahoo.com>
2024-01-30 17:23:15 +0100

    Add status icon to shipment history status text

--- app/src/main/java/com/mendhie/movemate/presentation/adapters/ShipmentAdapter.kt
+++ app/src/main/java/com/mendhie/movemate/presentation/adapters/ShipmentAdapter.kt
@@ -1,33 +1,56 @@
 package com.mendhie.movemate.presentation.adapters
 
+import android.R.attr.button
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.mendhie.movemate.R
 import com.mendhie.movemate.data.models.Shipment
 import com.mendhie.movemate.databinding.ItemShipmentBinding
 import com.mendhie.movemate.presentation.utils.Utils
 
+
 class ShipmentAdapter : ListAdapter<Shipment, ShipmentAdapter.ShipmentViewHolder>(Utils.ShipmentDiffUtilCallback){
 
     inner class ShipmentViewHolder(private val binding: ItemShipmentBinding): ViewHolder(binding.root){
 
         fun bindView(shipment: Shipment){
+            var img: Drawable = binding.root.context.resources.getDrawable(R.drawable.ic_check_circle_24px)
+
             binding.apply {
                 when(shipment.status){
-                    "in-progress" -> txtStatus.setTextColor(binding.root.context.resources.getColor(
-                        R.color.shipment_in_progress))
-                    "pending" -> txtStatus.setTextColor(binding.root.context.resources.getColor(
-                        R.color.shipment_pending))
-                    "loading" ->  txtStatus.setTextColor(binding.root.context.resources.getColor(
-                        R.color.shipment_loading))
+                    "in-progress" -> {
+                        txtStatus.setTextColor(
+                            binding.root.context.resources.getColor(
+                                R.color.shipment_in_progress
+                            )
+                        )
+                        img = binding.root.context.resources.getDrawable(R.drawable.ic_cached_24px)
+                    }
+                    "pending" -> {
+                        txtStatus.setTextColor(
+                            binding.root.context.resources.getColor(
+                                R.color.shipment_pending
+                            )
+                        )
+                        img = binding.root.context.resources.getDrawable(R.drawable.ic_history_sec_24px)
+                    }
+                    "loading" -> {
+                        txtStatus.setTextColor(
+                            binding.root.context.resources.getColor(
+                                R.color.shipment_loading
+                            )
+                        )
+                        img = binding.root.context.resources.getDrawable(R.drawable.ic_rotate_right_24px)
+                    }
                     "cancelled" ->  txtStatus.setTextColor(binding.root.context.resources.getColor(
                         R.color.shipment_cancelled))
                 }
                 txtStatus.text = shipment.status
                 txtAmount.text = shipment.amount
+                binding.txtStatus.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null)
             }
         }
     }
